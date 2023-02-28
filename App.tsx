20a580b696e3b24ef030bbc9b527c76b7f7a1ce4
Italo Lima <103470585+ilima12@users.noreply.github.com>
2023-02-28 00:27:33 -0300

    1:34:15 da aula

--- App.tsx
+++ App.tsx
@@ -1,16 +1,35 @@
 import { StatusBar } from 'react-native';
+import {
+  userFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter'
+
 import { Background } from './src/components/Background';
+import { useFonts } from '@expo-google-fonts/inter/useFonts';
 
 export default function App() {
+  const [fontsLoade] = useFonts({
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+
+  });
+
   return (
     <Background>
       
       <StatusBar
       barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
 
       
       />
-
+      {fontsLoaded ? }
     </Background>
 
   );
