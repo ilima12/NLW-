f33f528ead827be5985ddc7bec3335362df59b71
Italo Colt <103470585+ilima12@users.noreply.github.com>
2023-03-22 15:52:34 -0300

    Update index.tsx

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -2,10 +2,20 @@ import { TouchableOpacity, ImageBackground } from "react-native";
 
 import { styles } from './styles';
 
+export interface GameCardProps {
+    id: string;
+    name: string;
+    ads: string;
+    cover: ImageSourcePropType;
+}
+
 export function GameCard() {
     return (
         <TouchableOpacity style ={styles.container}>
-            <ImageBackground></ImageBackground>
+            <ImageBackground
+            style={styles.cover}
+            source={}
+            />
 
         </TouchableOpacity>
     );
